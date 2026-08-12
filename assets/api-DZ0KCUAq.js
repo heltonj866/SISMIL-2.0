46c819ebf205dbf0d0a7ea904616484be3c7efa9
heltonj866 <heltonj866@gmail.com>
2026-08-12 11:18:39 -0300

    Update compiled assets

---
+++ assets/api-DZ0KCUAq.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-C4c0irSa.js";async function t(t,n={}){let r=e(),i={...n.headers||{}};return r.csrfToken&&(i[`X-Csrf-Token`]=r.csrfToken,n.body instanceof FormData&&!n.body.has(`csrf_token`)&&n.body.append(`csrf_token`,r.csrfToken)),fetch(t,{...n,headers:i,credentials:`include`})}async function n(e,n={}){return(await t(e,n)).json()}export{n as t};
